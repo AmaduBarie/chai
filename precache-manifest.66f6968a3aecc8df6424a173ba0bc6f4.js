44a6df0ea217b293d85f73732f3ee45cfac30b86
amadubarie <amadubarie@gmail.com>
2020-02-16 02:34:58 +0000

    Updates

---
+++ precache-manifest.66f6968a3aecc8df6424a173ba0bc6f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b7add7ce00d4b4aa07cce419c46738",
+    "url": "/chai/index.html"
+  },
+  {
+    "revision": "41b4b9652c2062a46e10",
+    "url": "/chai/static/css/main.98daaf60.chunk.css"
+  },
+  {
+    "revision": "ca0d113028b326952a79",
+    "url": "/chai/static/js/2.5ca99518.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/chai/static/js/2.5ca99518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41b4b9652c2062a46e10",
+    "url": "/chai/static/js/main.f7042a49.chunk.js"
+  },
+  {
+    "revision": "212f3b8312b3a4ec08c4",
+    "url": "/chai/static/js/runtime-main.881d4824.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chai/static/media/logo.5d5d9eef.svg"
+  }
+]);
